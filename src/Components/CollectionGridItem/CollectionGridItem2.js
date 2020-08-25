f152224dfa9e4357ebd86a575e0ffdaf7830aa89
John.Huang <john.huang@onelab.tw>
2020-08-25 22:14:49 +0800

    force some img sizes

--- src/Components/CollectionGridItem/CollectionGridItem2.js
+++ src/Components/CollectionGridItem/CollectionGridItem2.js
@@ -2,14 +2,16 @@ import React from "react";
 import PP from "../../MockData/100001.jpg";
 import PW from "../../MockData/100002.jpg";
 import SG from "../../MockData/100003.jpg";
+import PG from "../../MockData/100004.png";
 
-import "./CollectionGridItem.scss";
+import "./CollectionGridItem2.scss";
 import AddToList from "../AddToList/AddToList";
 
 const BANNER_GALLERY = [
   PP,
   PW,
   SG,
+  PG
 ];
 
 const CollectionGridItem2 = ({ item, listItems, addItem }) => {
@@ -17,14 +19,14 @@ const CollectionGridItem2 = ({ item, listItems, addItem }) => {
   return (
     <div className="grid-item">
       <img
-        src={BANNER_GALLERY[id - 100001]}
+        src={BANNER_GALLERY[id - 100000]}
         alt="movie"
-        className="grid-item__img"
+        className="grid-item__img_demo"
       />
       <img
-        src={BANNER_GALLERY[id - 100001]}
+        src={BANNER_GALLERY[id - 100000]}
         alt="movie"
-        className="grid-item__img grid-item__mobile-img"
+        className="grid-item__img_demo grid-item__mobile-img_demo"
       />
       <div className={`${title ? "grid-item__movie-text" : "grid-item__tv-text"}`}>
         <h1 className="grid-item__title">{title}</h1>
